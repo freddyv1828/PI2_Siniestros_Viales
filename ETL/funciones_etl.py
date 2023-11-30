d720cfa759ab2d15be6db201d40724ade713e25e
freddyv1828 <freddy.valero88@gmail.com>
2023-11-30 00:16:53 -0400

    Añadir cambios

---
+++ ETL/funciones_etl.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import datetime as dt
+import re
+import numpy as np
+
+# Verificar tipo de dtos de los dataframe
+def verificar_datos(df):
+    # Comprobamos que el dataframe sea valido
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("El parámetro df, debe ser un dataframe de pandas")
+    
+    # Obtenemos un resumen de tipos de datos y valores nulos 
+    resume = {"columna": [], "tipo_dato": [], "datos_nulos": [],
+              "porcentaje_nulos": [], "porcentaje_no_nulos": []}
+    
+    for colum in df.columns:
+        no_nulos = (df[colum].count()/len(df)) * 100
+        # Advertimos si la columna tiene valores nulos
+        if df[colum].isnull().sum():
+            print(f"Advertencia: la columna {colum}, tiene valores nulos")
+            
+        resume["columna"].append(colum)
+        resume["tipo_dato"].append(df[colum].apply(lambda x: type(x)).unique())
+        resume["datos_nulos"].append(df[colum].isnull().sum())
+        resume["porcentaje_nulos"].append(round(100-no_nulos, 2))
+        resume["porcentaje_no_nulos"].append(round(no_nulos, 2))
+        
+    salida = pd.DataFrame(resume)
+    return salida
