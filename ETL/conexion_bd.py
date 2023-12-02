d3b261417e5d9cb0bf3240c0953282d608a17a8c
freddyv1828 <freddy.valero88@gmail.com>
2023-12-02 06:51:11 -0400

    Cambios 2/12/2023

---
+++ ETL/conexion_bd.py
@@ -0,0 +1,23 @@
+import pymysql
+from sqlalchemy import create_engine
+
+# Crear conexion a bd
+conexion = pymysql.connect(
+    host="localhost",
+    port= 3306,
+    user= "root",
+    passwd= "pupitre.123",
+    database= "siniestros_viales",
+)
+# Crear cursor
+cursor = conexion.cursor()
+
+# Crear conexio con create_engine
+host = "localhost"
+port = 3306
+user = "root"
+password = "pupitre.123"
+database = "siniestros_viales"
+
+conexion_string = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+engine = create_engine(conexion_string)
--- ETL/funciones_etl.py
+++ ETL/funciones_etl.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datetime as dt
 import re
 import numpy as np
+from datetime import datetime
 
 # Verificar tipo de dtos de los dataframe
 def verificar_datos(df):
@@ -27,3 +28,90 @@ def verificar_datos(df):
         
     salida = pd.DataFrame(resume)
     return salida
+
+# Función para encontrar valores duplicados
+def valores_duplicados(df, columnas):
+    # Se filtran las filas duplicadas
+    duplicated_rows = df[df.duplicated(subset = columnas, keep = False)]
+    
+    # Numero de filas duplicadas
+    numero_duplicados = duplicated_rows.shape[0]
+    
+    return numero_duplicados
+
+def verificar_tipo_variable(df):
+
+    mi_dict = {"nombre_campo": [], "tipo_datos": []}
+
+    for columna in df.columns:
+        mi_dict["nombre_campo"].append(columna)
+        mi_dict["tipo_datos"].append(df[columna].apply(type).unique())
+    df_info = pd.DataFrame(mi_dict)
+        
+    return df_info
+
+# Convierte hora a fotmato time
+def convertir_time(x):
+
+    if isinstance(x, str):
+        try:
+            return datetime.strptime(x, "%H:%M:%S").time()
+        except ValueError:
+            return None
+    elif isinstance(x, datetime):
+        return x.time()
+    return x
+
+# Inputar valores por la moda
+
+def imputa_valor_frecuente(df, columna):
+  
+    # Se reemplaza "SD" con NaN en la columna
+    df[columna] = df[columna].replace('SD', pd.NA)
+
+    # Se calcula el valor más frecuente en la columna
+    valor_mas_frecuente = df[columna].mode().iloc[0]
+    print(f'El valor mas frecuente es: {valor_mas_frecuente}')
+
+    # Se imputan los valores NaN con el valor más frecuente
+    df[columna].fillna(valor_mas_frecuente, inplace=True)
+    
+
+def ver_duplicados(df, columna):
+   
+    # Se filtran las filas duplicadas
+    duplicated_rows = df[df.duplicated(subset=columna, keep=False)]
+    if duplicated_rows.empty:
+        return "No hay duplicados"
+    
+    # se ordenan las filas duplicadas para comparar entre sí
+    duplicated_rows_sorted = duplicated_rows.sort_values(by=columna)
+    return duplicated_rows_sorted
+
+
+# Impita por valores frecuentes
+def imputa_valor_frecuente(df, columna):
+    # Se reemplaza "SD" con NaN en la columna
+    df[columna] = df[columna].replace('SD', pd.NA)
+
+    # Se calcula el valor más frecuente en la columna
+    valor_mas_frecuente = df[columna].mode().iloc[0]
+    print(f'El valor mas frecuente es: {valor_mas_frecuente}')
+
+    # Se imputan los valores NaN con el valor más frecuente
+    df[columna].fillna(valor_mas_frecuente, inplace=True)
+
+# Imputa valores de edad segun el sexo    
+def imputa_edad_media_segun_sexo(df):
+    
+    # Se reemplaza "SD" con NaN en la columna 'edad'
+    df['Edad'] = df['Edad'].replace('SD', pd.NA)
+
+    # Se calcula el promedio de edad para cada grupo de género
+    promedio_por_genero = df.groupby('Sexo')['Edad'].mean()
+    print(f'La edad promedio de Femenino es {round(promedio_por_genero["FEMENINO"])} y de Masculino es {round(promedio_por_genero["MASCULINO"])}')
+
+    # Se llenan los valores NaN en la columna 'edad' utilizando el promedio correspondiente al género
+    df['Edad'] = df.apply(lambda row: promedio_por_genero[row['Sexo']] if pd.isna(row['Edad']) else row['Edad'], axis=1)
+    # Lo convierte a entero
+    df['Edad'] = df['Edad'].astype(int)
